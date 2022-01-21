f0bb8363a8319a3596e556843194cd9291e21e63
jadjer <jadjer@ya.ru>
2022-01-21 10:45:08 +0300

    feat: Update logic show send message text

--- src/TelegramHandler.cpp
+++ src/TelegramHandler.cpp
@@ -19,16 +19,16 @@ void TelegramHandler::onCommandMarkMeAsAttendant(TgBot::Message::Ptr const& mess
 
   if (m_attendantChat == nullptr) {
     m_attendantChat = message->chat;
-    m_bot->getApi().sendMessage(message->chat->id, "Your mark as attendant");
+    m_bot->getApi().sendMessage(message->chat->id, "Your mark as attendant.");
     return;
   }
 
   if (message->chat->id == m_attendantChat->id) {
-    m_bot->getApi().sendMessage(message->chat->id, "Your already marked as attendant");
+    m_bot->getApi().sendMessage(message->chat->id, "Your already marked as attendant.");
 
   } else {
-    m_bot->getApi().sendMessage(message->chat->id, "Other human is as attendant");
-    m_bot->getApi().sendMessage(message->chat->id, "Attendant is " + m_attendantChat->firstName + " (@" + m_attendantChat->username + ")");
+    m_bot->getApi().sendMessage(message->chat->id, "Other human is as attendant.");
+    m_bot->getApi().sendMessage(message->chat->id, "Attendant is " + m_attendantChat->firstName + " (@" + m_attendantChat->username + ").");
   }
 }
 
@@ -36,16 +36,16 @@ void TelegramHandler::onCommandUnmarkMeAsAttendant(TgBot::Message::Ptr const& me
   logger::log_info("unmark_me_as_attendant");
 
   if (m_attendantChat == nullptr) {
-    m_bot->getApi().sendMessage(message->chat->id, "Your not marked as attendant");
+    m_bot->getApi().sendMessage(message->chat->id, "Your not marked as attendant.");
     return;
   }
 
   if (message->chat->id == m_attendantChat->id) {
     m_attendantChat = nullptr;
-    m_bot->getApi().sendMessage(message->chat->id, "Your unmark as attendant");
+    m_bot->getApi().sendMessage(message->chat->id, "Your unmark as attendant.");
 
   } else {
-    m_bot->getApi().sendMessage(message->chat->id, "Your not marked as attendant");
+    m_bot->getApi().sendMessage(message->chat->id, "Your not marked as attendant.");
   }
 }
 
@@ -53,28 +53,33 @@ void TelegramHandler::onCommandWhoIsAttendant(TgBot::Message::Ptr const& message
   logger::log_info("who_is_attendant");
 
   if (m_attendantChat == nullptr) {
-    m_bot->getApi().sendMessage(message->chat->id, "Attendant is not assigned");
+    m_bot->getApi().sendMessage(message->chat->id, "Attendant is not assigned.");
     return;
   }
 
-  m_bot->getApi().sendMessage(message->chat->id, "Attendant is " + m_attendantChat->firstName + " (@" + m_attendantChat->username + ")");
+  m_bot->getApi().sendMessage(message->chat->id, "Attendant is " + m_attendantChat->firstName + " (@" + m_attendantChat->username + ").");
 }
 
 void TelegramHandler::onCommandSetAttendantIs(TgBot::Message::Ptr const&) {}
 
 void TelegramHandler::onNonCommandMessage(TgBot::Message::Ptr const& message) {
   if (m_attendantChat == nullptr) {
-    m_bot->getApi().sendMessage(message->chat->id, "Attendant is not assigned");
+    m_bot->getApi().sendMessage(message->chat->id, "Attendant is not assigned.");
     return;
   }
 
   if (message->chat->id == m_attendantChat->id) {
-    m_bot->getApi().sendMessage(message->chat->id, "Your are attendant. The message has not been send");
+    m_bot->getApi().sendMessage(message->chat->id, "The message has not been send. Your are attendant.");
     return;
   }
 
   logger::log_info("message_from_{}", message->chat->username);
 
-  m_bot->getApi().sendMessage(message->chat->id, "The message has been send");
-  m_bot->getApi().forwardMessage(m_attendantChat->id, message->chat->id, message->messageId);
+
+  auto sentMessage = m_bot->getApi().forwardMessage(m_attendantChat->id, message->chat->id, message->messageId);
+  if (sentMessage) {
+    m_bot->getApi().sendMessage(message->chat->id, "The message has been send.");
+  } else {
+    m_bot->getApi().sendMessage(message->chat->id, "The message has not been send.");
+  }
 }
